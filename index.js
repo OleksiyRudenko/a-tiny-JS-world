b0331d94cd8196d125e4138707026785977d1c94
Zihfred <lichkingblizzard@gmail.com>
2018-11-29 17:57:54 +0200

    Fixed

--- index.js
+++ index.js
@@ -8,6 +8,34 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+class Inhabitant{
+        constructor(type,name,gender,legs,hands,say) {
+            this.type = type;
+            this.name = name;
+            this.gender = gender;
+            this.legs = legs;
+            this.hands = hands;
+            this.say = say;
+            this.getInfo = function () {
+                return this.type+";"+this.name+";"+this.gender+";"+this.legs+";"+this.hands+";"+ this.say;
+            }
+        }
+
+}
+
+const man = new Inhabitant('man','James','male',2,2,'Hello KOTTANS');
+const woman = new Inhabitant('woman', 'Alisa','female',2,2,'I hate this world');
+const dog = new Inhabitant('dog','Basta','male',4,0,'af-af');
+const cat = new Inhabitant('cat','Marpha','female',4,0,'meow-meow');
+
+print(man.getInfo());
+print(woman.getInfo());
+print(dog.getInfo());
+print(cat.getInfo());
+
+
+
+
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
