80fa600d1576e2fc8bdc3f370f46f6181e13fdb0
juliettswifty <yuliakatmuse@gmail.com>
2019-01-09 22:34:53 +0200

    add inhabitans and method to display it

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+const catSay = 'meow';
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+const dog = {
+    species: 'dog',
+    name: 'Toby',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'woof-woof!',
+};
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const cat = {
+    species: 'cat',
+    name: 'Markiz',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: catSay,
+    friends: ['Mouse', 'Tom'],
+};
 
+const woman = {
+    species: 'human',
+    name: 'Sarah',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: 'hey, sweetheart!',
+    friends: ['Rex', 'Tom', 'Jenn'],
+};
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const man = {
+    species: 'human',
+    name: 'Joe',
+    gender: 'male',
+    legs: 2,
+    hands: 2,
+    saying: 'hello there',
+};
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const catWoman = {
+    species: 'cat-woman',
+    name: 'Cat Woman',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: catSay,
+};
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+const listObj = (obj) => {
+    let str = '';
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+    for (item in obj) {
+        if (obj[item] !== 0) str += `${obj[item]}; `;
+    }
 
+    return str;
+};
 
+print(listObj(dog));
+print(listObj(cat));
+print(listObj(woman));
+print(listObj(man));
+print(listObj(catWoman));
