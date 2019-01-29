27fe6bf3d439b254c8e121e52b8806d358c5c5d4
Rostyk Broslavsky <broslavsky.rostislaw@gmail.com>
2019-01-29 18:43:58 +0200

    part-1

--- index.js
+++ index.js
@@ -29,3 +29,46 @@
    */
 
 
+'use strict';
+
+const dog = {
+  species: 'dog',
+  name: 'Toby',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof!'
+};
+
+const cat = {
+  species: 'cat',
+  name: 'Molly',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'meow'
+};
+
+const woman = {
+  species: 'human',
+  name: 'Jenny',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'Hello Chris!'
+};
+
+const man = {
+  species: 'human',
+  name: 'John',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'Hello Jenny!'
+};
+
+let creatures = [dog, cat, woman, man];
+
+creatures.forEach(el => {
+  print(`Hi! I am ${el.species}; My name is <strong>${el.name}</strong>; My gender is <em>${el.gender}</em>;I have ${el.legs} legs;I have ${el.hands} hands;I want to say ${el.saying};`);
+});
