7493aba3cd0a605ee376fb874a06dae79e8aa2aa
serhii.shramko <shramko.web@yahoo.com>
2020-10-08 22:37:03 +0300

    feat: add js-pre-oop task realization

--- index.js
+++ index.js
@@ -5,8 +5,57 @@
    Web app: _put project's github pages URL here_
    */
 
+const SPECIES = {
+    dog: 'dog',
+    cat: 'cat',
+    human: 'human'
+};
+
+const GENDER = {
+    male: 'male',
+    female: 'female',
+}
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+    species: SPECIES.dog,
+    legs: 4,
+    hands: 0,
+    name: 'Lassie',
+    gender: GENDER.female,
+    say: 'Woof woof',
+    friends: ['Donald'],
+};
+
+const cat = {
+    species: SPECIES.cat,
+    legs: 4,
+    hands: 0,
+    name: 'Garfield',
+    gender: GENDER.male,
+    say: 'Meow meow',
+    friends: ['Donald', 'Rosa Parks'],
+};
+
+const man = {
+    species: SPECIES.human,
+    legs: 2,
+    hands: 2,
+    name: 'Donald',
+    gender: GENDER.male,
+    say: 'Need more money',
+    friends: [],
+};
+
+const woman = {
+    species: SPECIES.human,
+    legs: 2,
+    hands: 2,
+    name: 'Rosa Parks',
+    gender: GENDER.female,
+    say: 'Black lives matter',
+    friends: ['Garfield', 'Lassie'],
+};
 
 
 // ======== OUTPUT ========
@@ -27,5 +76,14 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
+
+const inhabitantToString = ({ friends, name, gender, hands, legs, species, say }) => {
+    const friendsString = friends.length ? friends.join(', ') : 'None';
+
+    return `My name is ${name}. I am a ${gender} and i have ${hands} hands and ${legs} legs. My species is ${species}.
+I say: '${say}', and have this friends: ${friendsString}`
+}
+
+[man, dog, woman, cat].forEach((inhabitant) => print(inhabitantToString(inhabitant)));
 
 
