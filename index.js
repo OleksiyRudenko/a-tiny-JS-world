4a3a0ac46f10b80be363c3fd458c205981395228
Dmytro <dmytryj.kyzyma@gmail.com>
2022-08-10 15:40:45 +0300

    add tiny-js script

--- index.js
+++ index.js
@@ -28,4 +28,74 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
 
+   const dog = {
+      species: 'dog',
+      name: 'Toby',
+      gender: 'male',
+      legs: 4,
+      hands: 0,
+      saying: 'woof-woof!'
+    };
 
+    const cat = {
+      species: 'cat',
+      name: 'Mike',
+      gender: 'male',
+      legs: 4,
+      hands: 0,
+      saying: 'Murrrrrrrrrrr...'
+    };
+
+   const manJake = {  
+      species: 'human',
+      name: 'Jake',
+      gender: 'male',
+      legs: 2,
+      hands: 2,
+      saying: 'Hey, whats up?'
+   };
+
+   const manSpensor = {  
+      species: 'human',
+      name: 'Spensor',
+      gender: 'male',
+      legs: 2,
+      hands: 2,
+      saying: 'The weather is fine'
+   };
+   
+   const womanJessy = {
+      species: 'human',
+      name: 'Jessy',
+      gender: 'female',
+      legs: 2,
+      hands: 2,
+      saying: 'Im a little tired today'
+   };
+
+   const womanMargo = {
+      species: 'human',
+      name: 'Margo',
+      gender: 'male',
+      legs: 2,
+      hands: 2,
+      saying: 'Your shirt is just rubbish!'
+   };
+
+   const catWoman = {
+      species: 'Catwoman',
+      name: 'Kate',
+      gender: 'male',
+      legs: 2,
+      hands: 2,
+      saying: cat.saying
+   };
+
+   const arrayOfInhabitants = [dog, cat, manJake, manSpensor, womanJessy, womanMargo, catWoman];
+   const keyOfInhabitants = ["species", "name", "gender", "legs", "hands", "saying"];
+   let summaryArrayWithInhabitans = [];
+
+   arrayOfInhabitants.forEach(habitant => summaryArrayWithInhabitans.push(keyOfInhabitants.map(keyInObject => habitant[keyInObject])));
+
+   summaryArrayWithInhabitans.forEach(habitants => print(habitants.join("; ")));
+   
