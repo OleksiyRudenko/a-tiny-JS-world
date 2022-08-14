587ef6c7d2a47e0446f1ae001a03e84a90200435
LuckyDnepr <LuckyDnepr@gmail.com>
2022-08-14 17:37:29 +0300

    Create a JS Tiny World
    Edit style.css, index.html, index.js.
    Add information to worlds.md.

--- index.js
+++ index.js
@@ -1,31 +1,137 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+"use strict";
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+const inhabitants = initializeInhabinants();
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+for (const resident in inhabitants) {
+  print(getPrintableInhabitantInfo(inhabitants[resident]));
+}
 
+function initializeInhabinants() {
+  const inhabitants = {
+    man: {
+      species: "human",
+      name: "John",
+      gender: "male",
+      legs: 2,
+      hands: 2,
+      phrase: "Hi all! I'm a man!",
+      sayHi() {
+        return this.phrase;
+      },
+      friends: [],
+      enemies: [],
+    },
+    woman: {
+      species: "human",
+      name: "Mary",
+      gender: "female",
+      legs: 2,
+      hands: 2,
+      phrase: "Hi all! I'm a woman!",
+      sayHi() {
+        return this.phrase;
+      },
+      friends: [],
+      enemies: [],
+    },
+    dog: {
+      species: "dog",
+      name: "Maylo",
+      gender: "male",
+      legs: 4,
+      phrase: "Woof, woof!",
+      sayHi() {
+        return this.phrase;
+      },
+      friends: [],
+      enemies: [],
+    },
+    cat: {
+      species: "cat",
+      name: "Willow",
+      gender: "female",
+      legs: 4,
+      phrase: "Nyav, nyav!",
+      sayHi() {
+        return this.phrase;
+      },
+      friends: [],
+      enemies: [],
+    },
+    womanCat: {
+      species: "womanCat",
+      name: "Willowjane",
+      gender: "female",
+      legs: 2,
+      hands: 2,
+      friends: [],
+      enemies: [],
+    },
+  };
+  inhabitants.womanCat.sayHi = inhabitants.cat.sayHi.bind(inhabitants.cat);
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+  inhabitants.man.friends.push(inhabitants.woman);
+  inhabitants.woman.friends.push(inhabitants.man);
+  inhabitants.cat.friends.push(
+    inhabitants.man,
+    inhabitants.woman,
+    inhabitants.womanCat
+  );
+  inhabitants.dog.friends.push(inhabitants.man, inhabitants.woman);
+  inhabitants.womanCat.friends.push(
+    inhabitants.man,
+    inhabitants.woman,
+    inhabitants.cat
+  );
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+  inhabitants.woman.enemies.push(inhabitants.womanCat);
+  inhabitants.cat.enemies.push(inhabitants.dog);
+  inhabitants.dog.enemies.push(inhabitants.cat, inhabitants.womanCat);
+  inhabitants.womanCat.enemies.push(inhabitants.dog);
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+  return inhabitants;
+}
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+function getPrintableInhabitantInfo(inhabitant) {
+  const infoOneFields = ["species", "name", "gender", "legs", "hands"],
+    infoArrayFields = ["friends", "enemies"],
+    infoFunctionFields = ["sayHi"];
 
+  return [
+    ...infoOneFields.map((field) => {
+      if (inhabitant.hasOwnProperty(field)) {
+        return makePrintableSpan(inhabitant[field], field);
+      } else {
+        return makePrintableSpan("0", field);
+      }
+    }),
+    ...infoArrayFields.map((field) => {
+      if (inhabitant.hasOwnProperty(field)) {
+        return makePrintableListOfSpan(inhabitant[field], field);
+      } else {
+        return makePrintableSpan("0", field);
+      }
+    }),
+    ...infoFunctionFields.map((field) => {
+      if (inhabitant.hasOwnProperty(field)) {
+        return makePrintableSpan(inhabitant[field](), field);
+      } else {
+        return makePrintableSpan("0", "field");
+      }
+    }),
+  ].join(", ");
+}
 
+function makePrintableListOfSpan(relationTargets, cssClass) {
+  if (relationTargets.length != 0) {
+    return relationTargets.map(
+      (resident) => `<span class=${cssClass}>${resident.name}</span>`
+    );
+  } else {
+    return makePrintableSpan("0", cssClass);
+  }
+}
+
+function makePrintableSpan(text, cssClass) {
+  return `<span class=${cssClass}>${text}</span>`;
+}
