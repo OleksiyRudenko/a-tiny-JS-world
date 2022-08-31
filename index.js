dcab8f627ce4b788f6e04382ddad661b647fc9fc
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2022-08-31 18:42:23 +0300

    Defined the objects and output the data via print()

--- index.js
+++ index.js
@@ -7,25 +7,45 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+  species: "dog",
+  legs: 4,
+  hands: 0,
+  name: "Molly",
+  gender: "female",
+  saying: "WOOF-WOOF!",
+};
 
+const cat = {
+  species: "cat",
+  legs: 4,
+  hands: 0,
+  name: "Whiskey",
+  gender: "male",
+  saying: "Meow!",
+};
 
+const woman = {
+  species: "human",
+  legs: 2,
+  hands: 2,
+  name: "Kate",
+  gender: "female",
+  saying: "Have a good day!",
+};
+
+const man = {
+  species: "human",
+  legs: 2,
+  hands: 2,
+  name: "Dimitrij",
+  gender: "male",
+  saying: "Nice to meet you!",
+};
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const allInhabitants = [dog, cat, woman, man];
+const keys = Object.keys(allInhabitants[0]);
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+allInhabitants.map((inhabitan) => {
+  print(keys.map((key) => inhabitan[key]).join("; "));
+});
