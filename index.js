81d48a234043561535fbdc82f067d8c263f7bd29
Ivan Chukhalo <ivanchukhalo@gmail.com>
2022-09-01 15:37:43 +0300

    feat: add friends property to classes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 class Creature {
-   constructor (name, sex){
+   constructor (name, sex, friendsArr = [{name: 'Forever alone'}]){
       this.name = name;
       if (sex !== 'Male' && sex !== 'Female'){
          this.sex = 'WARNING! Invalid value of a sex property';
       } else {
          this.sex = sex;
       }
+      this.friends = [];
+      friendsArr.forEach((inhabitant)=>{
+         this.friends.push(inhabitant.name);
+      })
+      this.friends = this.friends.join(', ')
    }
 
    getInfo(){
@@ -18,39 +23,39 @@ class Creature {
 }
 
 class Human extends Creature{
-   constructor(name, sex, saying){
-      super(name, sex);
+   constructor(name, sex, friendsArr, saying){
+      super(name, sex, friendsArr);
       this.species = 'Human';
+      this.saying = saying;
       this.legs = 2;
       this.hands = 2;
-      this.saying = saying;
    }
 }
 
 class Dog extends Creature{
-   constructor (name, sex){
-      super(name, sex);
+   constructor (name, sex, friendsArr){
+      super(name, sex, friendsArr);
       this.species = 'Dog';
+      this.saying = 'Woof';
       this.legs = 4;
       this.hands = 0;
-      this.saying = 'Woof';
    }
 }
 
 class Cat extends Creature {
-   constructor (name, sex){
-      super(name, sex);
+   constructor (name, sex, friendsArr){
+      super(name, sex, friendsArr);
       this.species = 'Cat';
+      this.saying = 'Meow';
       this.legs = 4;
       this.hands = 0;
-      this.saying = 'Meow';
    }
 }
 
-let janLevinson = new Human ('Jan Levinson', 'Female', "You are not the first one!")
-let michaelScott = new Human ('Michael Scott', 'Male', "That's what she said!");
-let theirDog = new Dog ('Sobachka', 'Female');
-let theirCat = new Cat ('Kytsya', 'Female');
+let janLevinson = new Human ('Jan Levinson', 'Female', undefined, "You are not the first one!")
+let michaelScott = new Human ('Michael Scott', 'Male', [janLevinson], "That's what she said!");
+let theirDog = new Dog ('Sobachka', 'Female', [janLevinson]);
+let theirCat = new Cat ('Kytsya', 'Female', [janLevinson, theirDog]);
 
 print(janLevinson.getInfo());
 print(michaelScott.getInfo());
