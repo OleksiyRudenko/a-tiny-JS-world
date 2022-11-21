cabe2eb89101461af336d45613afdcad520bf225
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-11-21 17:30:41 +0200

    [populate-world] code completed

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 import { print } from './js/lib.js';
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+class Inhabitant{
+   constructor(type, gender, name, say){
+      this.type = type;
+      this.gender = gender;
+      this.name = name;
+      this.say = say;
+      this.properties = ["type", "gender", "name", "say"];
+   }
+   toString(){
+      return this.properties
+         .map(item => this[item])
+         .join('; ');
+   }
+}
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+class Animal extends Inhabitant{
+   constructor(type, gender, name, say){
+      super(type, gender, name, say);
+      this.legs = 4;
+      this.properties = [...this.properties, 'legs'];
+   }
+}
 
+class Dog extends Animal{
+   constructor(gender, name){
+      super('Dog', gender, name, 'woof-woof');
+   }
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+class Cat extends Animal{
+   constructor(gender, name){
+      super('Cat', gender, name, 'meow');
+   }
+}
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+class Human extends Inhabitant{
+   constructor(gender, name, say){
+      super('Human', gender, name, say);
+      this.legs = 2;
+      this.hands = 2;
+      this.properties = [...this.properties, 'legs', 'hands'];
+   }
+}
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+class Male extends Human{
+   constructor(gender, name){
+      super(gender, name, "I'm a man");
+   }
+}
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+class Female extends Human{
+   constructor(gender, name){
+      super(gender, name, "I'm a woman");
+   }
+}
+
+const dog = new Dog('male', 'Tuzik');
+const cat = new Cat('female', 'Murka');
+const woman = new Female('female', 'Anna');
+const man = new Male('Male', 'Stephan');
+
+const inhabitants = [dog, cat, woman, man];
+
+inhabitants.forEach(elem => String(print(elem + ';')));
